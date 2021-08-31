a55dd3df3e190e46814173ecaee01418e8bda626
vladko <max.vlad.kor@gmail.com>
2021-08-31 17:59:07 +0300

    Implemented code which take parameters from url

--- app.js
+++ app.js
@@ -30,6 +30,29 @@ app.get('/api/v1/tours', (req, res) => {
 });
 
 
+app.get('/api/v1/tours/:id', (req, res) => {
+    console.log(req.params);
+
+    const id = parseInt(req.params.id);
+
+    if (id >  tours.length) {
+        return res.status(404).json({
+            status: 'Fail',
+            message: 'Invalid id'
+        })
+    }
+
+    const tour = tours.find(el => el.id === id);
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour
+        }
+    })
+});
+
+
 app.post('/api/v1/tours', (req, res) => {
     // console.log(req.body);
     const newId = tours[tours.length - 1].id + 1;
