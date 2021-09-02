d9dd3c823eb6d6ad3b0ac6e9a4a34744ee489320
vladko <max.vlad.kor@gmail.com>
2021-09-02 16:07:07 +0300

    Connected to local db? created simple scchema

--- server.js
+++ server.js
@@ -1,9 +1,37 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const PORT = process.env.PORT || 3000;
 
-dotenv.config({path:'./config.env'})
+dotenv.config({path: './config.env'})
+
+mongoose.connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(() => console.log('DB connection successful!'))
+    .catch(err => console.log(err))
+
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name!'],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price!']
+    }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 const app = require('./app');
+const {mongo} = require("mongoose");
 // console.log(process.env);
 
 app.listen(PORT, () => {
