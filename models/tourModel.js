66da624419b032d7582ad9781232f589aac3843f
vladko <max.vlad.kor@gmail.com>
2021-09-07 21:25:13 +0300

    Worked with document< query and aggregation middlewares

--- models/tourModel.js
+++ models/tourModel.js
@@ -54,7 +54,11 @@ const tourSchema = new mongoose.Schema({
         default: Date.now(),
         select: false
     },
-    startDates: [Date]
+    startDates: [Date],
+    secretTour: {
+        type: Boolean,
+        default: false
+    }
 }, {
     toJSON: {virtuals: true},
     toObject: {virtuals: true}
@@ -79,6 +83,29 @@ tourSchema.pre('save', function(next) {
 //     console.log(doc) ;
 //     next();
 // });
+
+// Query middleware
+tourSchema.pre(/^find/, function (next) {
+// tourSchema.pre('find', function (next) {
+    this.find({secretTour: {$ne: true}});
+
+    this.start = Date.now();
+    next()
+});
+
+tourSchema.post(/^find/, function(docs, next) {
+    console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+    console.log(docs);
+    next();
+});
+
+// Aggregation middleware
+tourSchema.pre('aggregate', function(next) {
+    this.pipeline().unshift({$match: {secretTour: {$ne: true}}});
+    console.log(this.pipeline());
+    next();
+})
+
 
 const Tour = mongoose.model('Tour', tourSchema);
 
