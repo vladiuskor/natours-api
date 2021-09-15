54829e25b06c4f46df55d6353fd25b5847691b72
vladko <max.vlad.kor@gmail.com>
2021-09-16 01:02:13 +0300

    Finally returned to work with authentication

--- server.js
+++ server.js
@@ -16,6 +16,24 @@ mongoose.connect(process.env.DATABASE_LOCAL, {
 
 const app = require('./app');
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`App running on port ${PORT}...`);
 });
+
+//Global unhandled rejections EXAMPLES
+process.on('unhandledRejection', err => {
+    console.log('Unhandled Rejection!!! Shutting down...');
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    })
+});
+
+//Global uncaught exceptions EXAMPLES
+process.on('uncaughtException', err => {
+    console.log('Uncaught Exception!!! Shutting down...');
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    })
+})
