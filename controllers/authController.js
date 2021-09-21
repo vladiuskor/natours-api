20810edcaf6d68552f54c47989939f60b43c94ba
vladko <max.vlad.kor@gmail.com>
2021-09-21 14:13:28 +0300

    Added stuff wich crush all

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,7 +13,6 @@ const signToken = id => {
     });
 }
 
-
 const signUp = catchAsync(async (req, res, next) => {
 
     const newUser = await User.create({
@@ -158,11 +157,30 @@ const resetPassword = catchAsync(async (req, res, next) => {
     });
 });
 
+// const updatePassword = catchAsync(async (req, res, next) => {
+//     // 1) Get user from the collection
+//     const user = await User.findById(req.user.id).select('+password');
+//
+//     // 2) Check if POSTed current password is correct
+//     if(!( await user.correctPassword(req.body.passwordConfirm, user.password))) {
+//         return next(new AppError('Your current password is wrong', 401))
+//     }
+//     // 3) If so, update password
+//     user.password = req.body.password;
+//     user.passwordConfirm = req.body.passwordConfirm;
+//     await user.save();   // REMEMBER!!!  User.findByIdAndUpdate will not work as intended! (Won't work validation on schema and pre-middlewares)
+//
+//     // 4) Log user in, send JWT
+//
+//
+// });
+
 module.exports = {
     signUp,
     logIn,
     protect,
     restrictTo,
     forgotPassword,
-    resetPassword
+    resetPassword,
+    // updatePassword
 }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -9,16 +9,21 @@ const {
 const {
     signUp,
     logIn,
+    protect,
     forgotPassword,
-    resetPassword
+    resetPassword,
+    updatePassword
 } = require('./../controllers/authController');
 
 const router = express.Router();
 
 router.post('/signup', signUp);
 router.post('/login', logIn);
+
 router.post('/forgotPassword', forgotPassword);
 router.patch('/resetPassword/:token', resetPassword);
+
+// router.patch('/updateMyPassword', protect, updatePassword);
 
 router.route('/')
     .get(getAllUsers)
