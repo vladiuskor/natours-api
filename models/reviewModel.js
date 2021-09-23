469fe5c8b1d3304bbdcc8ed342d6f1c3ee94b040
vladko <max.vlad.kor@gmail.com>
2021-09-23 18:12:10 +0300

    Implemented reviews controller and router

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -14,7 +14,7 @@ const reviewSchema = new mongoose.Schema({
     },
     createdAt: {
         type: Date,
-        default: Date.now(),
+        default: Date.now,
         // select: false
     },
     tour: {
