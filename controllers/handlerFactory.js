e4f4f0dfb80ca82890e70854b8744db0350072de
vladko <max.vlad.kor@gmail.com>
2021-09-27 21:26:39 +0300

    Updated controllers

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -76,6 +76,7 @@ exports.getAll = (Model) => catchAsync(async (req, res, next) => {
         .sort()
         .limitFields()
         .paginate();
+    // const doc = await features.query.explain();
     const doc = await features.query;
 
     // SEND RESPONSE
--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('./../../models/tourModel');
+const User = require('./../../models/userModel');
+const Review = require('./../../models/reviewModel');
 
 dotenv.config({path: './config.env'})
 
@@ -15,11 +17,16 @@ mongoose.connect(process.env.DATABASE, {
 
 // Read JSON-file
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'));
 
 // Import data to DB
 const importData =  async () => {
    try {
        await Tour.create(tours);
+       await User.create(users, {validateBeforeSave: false});
+       await Review.create(reviews);
+
        console.log('Data successfully loaded!');
        process.exit(); /*Don't use process.exit() in big projects!!! Only for small scripts!!! */
    }  catch (err) {
@@ -31,6 +38,8 @@ const importData =  async () => {
 const deleteData = async () => {
     try {
         await Tour.deleteMany();
+        await User.deleteMany();
+        await Review.deleteMany();
         console.log('Data successfully deleted!');
         process.exit(); /*Don't use process.exit() in big projects!!! Only for small scripts!!! */
     }  catch (err) {
