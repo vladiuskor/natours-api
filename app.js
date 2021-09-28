dac378568471e7ae071308aa543da7e74314196b
vladko <max.vlad.kor@gmail.com>
2021-09-28 17:05:14 +0300

    Added extends to basr.pug

--- app.js
+++ app.js
@@ -72,9 +72,21 @@ app.use((req, res, next) => {
 // 3) ROUTES
 app.get('/', (req, res) => {
    res.status(200).render('base', {
-       tour: 'Forest Trip',
+       tour: 'The Forest Hiker',
        user: 'Jonas'
    });
+});
+
+app.get('/overview', (req, res) => {
+    res.status(200).render('overview', {
+        title: 'All tours'
+    });
+});
+
+app.get('/tour', (req, res) => {
+    res.status(200).render('tour', {
+        title: 'The Forest Hiker Tour'
+    });
 });
 
 app.use('/api/v1/tours', tourRouter);
