ae25ec2d1fd85f63cea7f690a6fd7587f2e923c2
vladko <max.vlad.kor@gmail.com>
2021-10-04 20:58:35 +0300

    Added simple logic to work with simple forms

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -9178,7 +9178,7 @@ var _login = require("./login");
 
 // DOM ELEMENTS
 var mapBox = document.getElementById('map');
-var loginForm = document.querySelector('.form');
+var loginForm = document.querySelector('.form--login');
 var logOutBtn = document.querySelector('.nav__el--logout'); // DELEGATION
 
 if (mapBox) {
@@ -9226,7 +9226,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42081" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44573" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
